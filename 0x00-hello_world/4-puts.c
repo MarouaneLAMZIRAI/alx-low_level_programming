d31013d2bec4e8c69edb81e8f95d60ae77f0dc31
Your Name <you@example.com>
2023-08-10 16:18:17 -0700

    this committy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: This function prints a message using the puts function.
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * Description: this function print something
+ * Return: 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
