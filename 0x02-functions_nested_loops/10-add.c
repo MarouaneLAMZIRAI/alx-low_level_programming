7ab2895cc9fc19740b8c2da22f05b20b4e875e7d
Your Name <you@example.com>
2023-08-15 08:55:33 -0700

    this is an add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
