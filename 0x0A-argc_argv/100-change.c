1be4ef398148d858935a6906f2596594ce73d7df
Your Name <you@example.com>
2023-09-02 05:40:23 -0700

    last commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if any argument is not a non-negative integer
+ */
+int main(int argc, char *argv[])
+{
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int cents, i, count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
