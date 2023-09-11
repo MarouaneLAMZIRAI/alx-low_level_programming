b6293a8fae4dab566fb6237612535638b648c098
Your Name <you@example.com>
2023-09-11 11:53:00 -0700

    Forth commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /* Prototype for the print_dog function */
 void print_dog(struct dog *d);
 
+/* Define a new type dog_t as an alias for struct dog */
+typedef struct dog dog_t;
+
 
 #endif /* DOG_H */
