d855775566128d3f7bc51a82b0224f93f0e8c587
Your Name <you@example.com>
2023-09-13 06:25:12 -0700

    Second comm

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - execute a func given as a param on each element of an arr
  * @array: an array of integers
  * @size: the size of the array
  * @action: a pointer to the function to be executed on each element
